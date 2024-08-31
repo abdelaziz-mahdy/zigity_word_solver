a00822de6e1a2c1863e6ec77035bff7b32de7189
Abdelaziz Mahdy <zezohassam@gmail.com>
2024-08-31 15:06:25 -0300

    fixes the service issue

--- lib/services/word_service.dart
+++ lib/services/word_service.dart
@@ -5,8 +5,10 @@ class WordService {
   Set<String> _wordSet = {};
 
   Future<void> loadWords() async {
+    // const url =
+    //     'https://raw.githubusercontent.com/dwyl/english-words/master/words.txt';
     const url =
-        'https://raw.githubusercontent.com/dwyl/english-words/master/words.txt';
+        "https://raw.githubusercontent.com/dolph/dictionary/master/popular.txt";
     try {
       final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
@@ -20,18 +22,14 @@ class WordService {
 
   List<String> findWords(List<String> mandatoryLetters,
       List<String> availableLetters, int freeLetters) {
-    String inputLetters = availableLetters.join('').toLowerCase();
-    if (mandatoryLetters.isEmpty && inputLetters.isEmpty) return [];
+    if (mandatoryLetters.isEmpty && availableLetters.isEmpty) return [];
 
     List<String> foundWords = _wordSet
-        .where((word) => _canFormWord(
-            word, mandatoryLetters.join(''), inputLetters, freeLetters))
+        .where((word) =>
+            _canFormWord(word, mandatoryLetters, availableLetters, freeLetters))
         .toList();
 
-    foundWords.sort((a, b) => _countUsedLetters(
-            b, mandatoryLetters.join('') + inputLetters)
-        .compareTo(
-            _countUsedLetters(a, mandatoryLetters.join('') + inputLetters)));
+    foundWords.sort((b, a) => a.length.compareTo(b.length));
 
     if (foundWords.length > 5) {
       foundWords = foundWords.sublist(0, 5); // Limit to top 5 words
@@ -40,43 +38,48 @@ class WordService {
     return foundWords;
   }
 
-  bool _canFormWord(String word, String mandatoryLetters, String inputLetters,
-      int freeLetters) {
-    for (var letter in mandatoryLetters.split('')) {
+  bool _canFormWord(String word, List<String> mandatoryLetters,
+      List<String> availableLetters, int freeLetters) {
+    // Ensure the word contains all mandatory letters
+    for (var letter in mandatoryLetters) {
       if (!word.contains(letter)) return false;
     }
 
-    Map<String, int> letterCount = {};
-    for (var letter in inputLetters.split('')) {
+    // Count the letters in the input available letters
+    Map<String, int> letterCount = {mandatoryLetters.firstOrNull ?? "": 1};
+    for (var letter in availableLetters) {
       letterCount[letter] = (letterCount[letter] ?? 0) + 1;
     }
 
     int neededFreeLetters = 0;
 
     for (var letter in word.split('')) {
-      if (mandatoryLetters.contains(letter)) {
-        continue;
-      }
-
       if (letterCount.containsKey(letter) && letterCount[letter]! > 0) {
+        // Use an available letter if possible
         letterCount[letter] = letterCount[letter]! - 1;
       } else {
+        // If no more available letters, count it as a needed free letter
         neededFreeLetters++;
       }
     }
 
+    // Ensure the word can be formed with the available letters and free letters
     return neededFreeLetters <= freeLetters;
   }
 
-  int _countUsedLetters(String word, String allLetters) {
-    int count = 0;
-    Set<String> usedLetters = {};
-    for (var letter in word.split('')) {
-      if (allLetters.contains(letter) && !usedLetters.contains(letter)) {
-        count += word.split(letter).length - 1;
-        usedLetters.add(letter);
-      }
-    }
-    return count;
-  }
+  // int _countUsedLetters(String word, List<String> mandatoryLetters,
+  //     List<String> availableLetters) {
+  //   int count = 0;
+  //   Set<String> usedLetters = {};
+
+  //   List<String> allLetters = [...mandatoryLetters, ...availableLetters];
+
+  //   for (var letter in word.split('')) {
+  //     if (allLetters.contains(letter) && !usedLetters.contains(letter)) {
+  //       count += word.split(letter).length - 1;
+  //       usedLetters.add(letter);
+  //     }
+  //   }
+  //   return count;
+  // }
 }
